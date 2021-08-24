2569e281def65d1ca803c21c5e7df75e1e4b2942
gerson dos santos de souza <gersonsouza97@outlook.com>
2021-08-24 11:17:50 -0300

    setup do projeto

---
+++ src/main/java/br/com/academy/gerson/projetotransacao/ProjetotransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.academy.gerson.projetotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/academy/gerson/projetotransacao/ProjetotransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.gerson.projetotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetotransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
